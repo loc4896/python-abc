f2b54fbf566132921ebf3704b25e9bff5a17d53f
loc4896 <hcndloc@gmail.com>
2026-09-15 18:10:05 +0700

    app_localhost.py

--- app_localhost.py
+++ app_localhost.py
@@ -1326,5 +1326,7 @@ def congno_khachhang_web():
 # ==========================================================
 # CHẠY WEBSITE
 # ==========================================================
+import os
+
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
